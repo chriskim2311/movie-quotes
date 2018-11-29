bc408d074a11215815dcc4811e4c397f3731b407
chriskim2311 <chriskim2311@gmail.com>
2018-11-28 17:04:39 -0800

    completed fake sign in and sign out

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,43 +1,64 @@
-import React, {Component} from 'react';
-import {Link} from 'react-router-dom'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
+import { userSignIn, userSignOut } from '../actions';
+
 
 class Nav extends Component {
 
-    render(){
+    renderLinks(){
+        const {auth, signIn, signOut} = this.props;
+        if(auth) {
+            return <button onClick={signOut} className= "yellow btn darken-2">Sign Out</button>
+        }
+
+        return <button onClick={signIn} className= "btn red">Sign In</button>
+    }
+
+    render() {
         const navStyle = {
             padding: '0 8px'
         }
-        return(
-        <nav style = {navStyle} className="blue darken-2">
-            <div className="nav-wrapper">
-            <Link to='/' className="brand-logo">Movie Quotes!</Link>
-            <ul className= "right">
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-                <li>
-                    <Link to="/public-list">Public List</Link>
-                </li>
-                <li>
-                    <Link to="/secret-list">Secret List</Link>
-                </li>
-                <li>
-                    <Link to="/quotes">Quotes</Link>
-                </li>
-                <li>
-                    <Link to="/sign-in">Sign In</Link>
-                </li>
-                <li>
-                    <Link to="/sign-up">Sign Up</Link>
-                </li>
-            </ul>
-            </div>
-        </nav>
+
+        return (
+            <nav style={navStyle} className="blue darken-2">
+                <div className="nav-wrapper">
+                    <Link to='/' className="brand-logo">Movie Quotes!</Link>
+                    <ul className="right">
+                        <li>
+                            <Link to="/">Home</Link>
+                        </li>
+                        <li>
+                            <Link to="/about">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/public-list">Public List</Link>
+                        </li>
+                        <li>
+                            <Link to="/secret-list">Secret List</Link>
+                        </li>
+                        <li>
+                            <Link to="/quotes">Quotes</Link>
+                        </li>
+                        <li>
+                           {this.renderLinks()}
+                        </li>
+                        <li>
+                            <Link to="/sign-up">Sign Up</Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
         )
     }
 }
+function mapStateToProps(state) {
+    return {
+        auth: state.user.auth
+    }
+}
 
-export default Nav;
+export default connect(mapStateToProps, {
+    signIn: userSignIn,
+    signOut: userSignOut
+})(Nav);
